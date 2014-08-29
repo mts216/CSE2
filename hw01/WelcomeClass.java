437539d5bd56ad6544a61c85b414f19b8fd9419f
mts216 <mts216@lehigh.edu>
2014-08-29 15:27:16 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////
+//Mackenzie Snow
+//hw01 
+//Welcome Class Java Program
+//
+
+//Define a class
+public class WelcomeClass{
+    
+    //Add main method
+    public static void main(String[ ] args){
+        
+        //print the statement
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("   ^  ^  ^  ^  ^  ^");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println(" |-M--T--S--2--1--6-|");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("   v  v  v  v  v  v");
+        System.out.println("I have a sticky note over my camera because it freaks me out that people could be watching me");
+        }
+}
